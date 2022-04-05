1acf52ec371c9707e21e7a1a121c82ab502200a6
Joseph Wobus <58497470+PerseusXD@users.noreply.github.com>
2022-04-04 23:58:51 -0400

    add caching

--- src/main/java/com/Joseph/CoinmarketcapAPI/CoinmarketcapApiApplication.java
+++ src/main/java/com/Joseph/CoinmarketcapAPI/CoinmarketcapApiApplication.java
@@ -2,8 +2,10 @@ package com.Joseph.CoinmarketcapAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CoinmarketcapApiApplication {
 
 	public static void main(String[] args) {
